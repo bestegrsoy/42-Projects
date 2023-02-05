f4af1b93beb040956fe24afd4aa65b1246156bf0
bestegrsoy <116671743+bestegrsoy@users.noreply.github.com>
2023-02-05 17:44:02 +0300

    Add files via upload

---
+++ libft.a/ft_strchr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgursoy <bgursoy@student.42kocaeli.com.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/29 19:22:25 by bgursoy           #+#    #+#             */
+/*   Updated: 2022/12/29 19:22:26 by bgursoy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include	"libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	s_len;
+
+	s_len = ft_strlen(s);
+	while (s_len >= 0)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+		s_len--;
+	}
+	return (NULL);
+}
